f1f7e3c87063e4ba5645f53e2db83166353511fe
Adrian Martino <25408358+awm333@users.noreply.github.com>
2025-04-04 11:12:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -304,14 +304,15 @@ def plot_cars(model_1, model_2, gas_price=3.15, kwh_price=0.12, grid_emissions_o
 
     #col1, col_space, col2 = st.columns([12,1,4])
     col1, col2 = st.columns([3.5,1])
+
     with col1:
-        
-        
+        st.markdown("<h2 style='text-align: center;'>Cost of Ownership</h2>", unsafe_allow_html=True)
         with st.expander(label = "How to Read This Cost of Ownership Chart"):
             st.write('This annotated image is an example Cost of Ownership Chart with the default settings:')
             st.image('How-To Mock Up.jpg')
-        st.markdown("<h2 style='text-align: center;'>Cost of Ownership</h2>", unsafe_allow_html=True)
         st.pyplot(fig1)
+
+        st.divider()
 
         st.markdown("<h2 style='text-align: center;'>Emissions</h2>", unsafe_allow_html=True)
         with st.expander(label = "How to Read This Emissions Chart"):
