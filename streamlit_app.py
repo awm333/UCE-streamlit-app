1a8fdf9be8186a2702b4cc6eb956f550a188acc9
Adrian Martino <25408358+awm333@users.noreply.github.com>
2025-04-06 12:14:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -390,7 +390,7 @@ def plot_cars(model_1, model_2, gas_price=3.15, kwh_price=0.12, grid_emissions_o
                   label_visibility = "visible",
                   help = f"Assuming 14 years of ownership--the [average age of cars passenger cars in operation]({vehicle_age_link}) in the United States")
     with met2_col2:
-        st.metric(label = "Breakeven: ",
+        st.metric(label = "Emissions Breakeven: ",
                   value = breakeven_value_emissions,
                   label_visibility = "visible",
                   help = "The year during which the running lifetime emissions of the EV is less than the ICEV")
